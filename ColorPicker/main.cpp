e3ad04d4a7b985728c057bcd49a26919f5c3ed1b
skotraba <57115133+skotraba@users.noreply.github.com>
2020-02-11 10:55:42 -0500

    added additional boxes to show harmonious colors

--- ColorPicker/main.cpp
+++ ColorPicker/main.cpp
@@ -1,6 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+#include "Text.h"
+
+
+/******************Need to add user instructions and Shader Option***********/
+
 
 //TODO Check if the color clicked is on the color triangle
 bool isValidColor(sf::Vector2i location)
@@ -16,16 +21,32 @@ bool isValidColor(sf::Vector2i location)
 //Set current color to current color clicked
 sf::Color getColor(int a, int g, int b)
 {
-	sf::Color color(a, g, b);
-	return color;
+	//sf::Color color(a, g, b);
+	return sf::Color(a, g, b);
+}
+
+sf::Color getColorFirst(int a, int g, int b)
+{
+	return sf::Color(g, b, a);
+
+}
+
+sf::Color getColorSecond(int a, int g, int b)
+{
+	return sf::Color(a, a, g);
 }
 
 
 int main()
 {
+
+
 	// create the window
-	sf::RenderWindow window(sf::VideoMode(800, 600), "My window", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Color Picker", sf::Style::Close);
+	
+	//Create texture
 	sf::Texture texture;
+	texture.create(800, 600);
 
 	//Create text
 	sf::Text text;
@@ -40,31 +61,60 @@ int main()
 	sf::Text title;
 	title.setString("Welcome to Color Picker!");
 	title.setCharacterSize(40);
+	title.setFillColor(sf::Color::Black);
 	title.setFont(font);
 	title.setPosition(100.f, 20.f);
 
-
+	//Labels
 	text.setString("Your choosen color!");
+	text.setFillColor(sf::Color::Black);
 	text.setCharacterSize(24);
-	text.setPosition(400.f, 200.f);
+	text.setPosition(400.f, 100.f);
+
+	sf::Text label2;
+	label2.setFont(font);
+	label2.setString("Additional Color");
+	label2.setFillColor(sf::Color::Black);
+	label2.setCharacterSize(24);
+	label2.setPosition(400.f, 200.f);
+
+	sf::Text label3;
+	label3.setFont(font);
+	label3.setString("Additional Color");
+	label3.setFillColor(sf::Color::Black);
+	label3.setCharacterSize(24);
+	label3.setPosition(400.f, 300.f);
+	
+
 	
 	
 	
 	//Create the current color choosen, default white
-	sf::RectangleShape rectangle;
-	rectangle.setPosition(400.f, 250.f);
-	rectangle.setSize(sf::Vector2f(40.f, 40.f));
-	
-	
-	//Triangle color picker
-	sf::VertexArray triangle(sf::Triangles, 3);
+	sf::RectangleShape rectangleMain;
+	rectangleMain.setPosition(400.f, 150.f);
+	rectangleMain.setSize(sf::Vector2f(40.f, 40.f));
 
-	triangle[0].position = sf::Vector2f(100.f, 350.f);
-	triangle[1].position = sf::Vector2f(350.f, 100.f);
-	triangle[2].position = sf::Vector2f(350.f, 350.f);
-	triangle[0].color = sf::Color::Red;
-	triangle[1].color = sf::Color::Blue;
-	triangle[2].color = sf::Color::Green;
+	//Additional color rectangle
+	sf::RectangleShape rectangle2;
+	rectangle2.setPosition(400.f, 250.f);
+	rectangle2.setSize(sf::Vector2f(40.f, 40.f));
+
+	//Additional color rectangle
+	sf::RectangleShape rectangle3;
+	rectangle3.setPosition(400.f, 350.f);
+	rectangle3.setSize(sf::Vector2f(40.f, 40.f));
+
+
+
+	sf::CircleShape circle(100);
+	circle.setPosition(50.f, 200.f);
+	sf::Texture circleTex;
+	circleTex.loadFromFile("image\\circleColor.png");
+	circle.setTexture(&circleTex);
+	/*circle.setOutlineColor(sf::Color::Black);
+	circle.setOutlineThickness(1);
+	*/
+
 
 
 	// run the program as long as the window is open
@@ -90,7 +140,9 @@ int main()
 				auto image = texture.copyToImage();
 				auto colorPoint = image.getPixel(position.x, position.y);
 				std::cout << (int)colorPoint.r << " " << (int)colorPoint.g << " " << (int)colorPoint.b << "\n";
-				rectangle.setFillColor(getColor(colorPoint.r, colorPoint.g, colorPoint.b));
+				rectangleMain.setFillColor(getColor(colorPoint.r, colorPoint.g, colorPoint.b));
+				rectangle2.setFillColor(getColorFirst(colorPoint.r, colorPoint.g, colorPoint.b));
+				rectangle3.setFillColor(getColorSecond(colorPoint.r, colorPoint.g, colorPoint.b));
 			}
 
 			
@@ -102,12 +154,16 @@ int main()
 
 		// draw everything here...
 		// window.draw(...);
-		window.draw(triangle);
-		texture.create(800, 600);
+		window.draw(circle);
+		
 		texture.update(window);
-		window.draw(rectangle);
+		window.draw(rectangleMain);
 		window.draw(title);
 		window.draw(text);
+		window.draw(label2);
+		window.draw(label3);
+		window.draw(rectangle2);
+		window.draw(rectangle3);
 
 		// end the current frame
 		window.display();
